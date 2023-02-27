2628b19571cf12f016927c9f50753966e70832e9
logben00 <89931163+logben00@users.noreply.github.com>
2023-02-26 21:43:27 -0700

    new

---
+++ counter app/index.js
@@ -0,0 +1,20 @@
+let incrementBtn = document.querySelector('#increment-btn')
+let saveBtn = document.querySelector('#save-btn')
+
+let count = 0
+let saveEl = document.querySelector("#save-el")
+let countEl = document.querySelector("#count-el")
+
+incrementBtn.onclick = function increment() {
+    count = count + 1
+    countEl.innerText = count
+}
+
+function save() {
+    let countStr = count + " - "
+    saveEl.innerText += countStr
+    countEl.textContent = 0
+    count = 0
+}
+
+console.log("Let's count people on the subway!")
