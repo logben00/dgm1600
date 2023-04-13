02958cba2a58b6a8c3d30ef794f1371e904a5a00
logben00 <89931163+logben00@users.noreply.github.com>
2023-04-13 09:13:43 -0600

    new

--- people/js/scripts.js
+++ people/js/scripts.js
@@ -1,63 +1,64 @@
-import { people } from "../data/people.js";
-import { getLastNumber, removeChildren } from "../utils/index.js";
+import { people } from "../people.js";
 
 const filterButtons = document.querySelector(".filterButtons");
 const characterDiv = document.querySelector(".characterDiv");
 const displayText = document.querySelector(".displayText");
 
-const allCharsButton = document.createElement("button");
-allCharsButton.textContent = "All Characters";
-allCharsButton.addEventListener("click", function () {
-  populateDOM(people);
-});
-
-const maleCharsButton = document.createElement("button");
-maleCharsButton.textContent = "Male Characters";
-maleCharsButton.addEventListener("click", function () {
-  const maleCharacters = people.filter((person) => person.gender === "male");
-  populateDOM(maleCharacters);
-});
-
-const femaleCharsButton = document.createElement("button");
-femaleCharsButton.textContent = "Female Characters";
-femaleCharsButton.addEventListener("click", function () {
-  const femaleCharacters = people.filter((person) => person.gender === "female");
-  populateDOM(femaleCharacters);
-});
-
-const otherCharsButton = document.createElement("button");
-otherCharsButton.textContent = "Other Characters";
-otherCharsButton.addEventListener("click", function () {
-  const otherCharacters = people.filter((person) => person.gender !== "male" && person.gender !== "female");
-  populateDOM(otherCharacters);
-});
+const allCharsButton = createFilterButton("All Characters", "all");
+const maleCharsButton = createFilterButton("Male Characters", "male");
+const femaleCharsButton = createFilterButton("Female Characters", "female");
+const otherCharsButton = createFilterButton("Other Characters", "other");
 
 filterButtons.appendChild(allCharsButton);
 filterButtons.appendChild(maleCharsButton);
 filterButtons.appendChild(femaleCharsButton);
 filterButtons.appendChild(otherCharsButton);
 
-function populateDOM(characters) {
-  removeChildren(characterDiv);
+function createFilterButton(text, filterValue) {
+  const button = document.createElement("button");
+  button.textContent = text;
+  button.addEventListener("click", function () {
+    if (filterValue != "all"){
+      const filteredChars = filterCharacters(filterValue);
+      populateDOM(filteredChars);
+    }else{
+      populateDOM(people);
+    }
+  });
+  return button;
+}
 
+function filterCharacters(filterValue) {
+  if (filterValue === "other") {
+    return people.filter((person) => person.gender !== "male" && person.gender !== "female");
+  } else {
+    return people.filter((person) => person.gender === filterValue);
+  }
+}
+
+function populateDOM(characters) {
+  characterDiv.innerHTML = "";
   if (characters.length === 0) {
-    const p = document.createElement("p");
-    p.textContent = "No matching characters found.";
-    characterDiv.appendChild(p);
+    displayText.textContent = "No matching characters found.";
   } else {
     characters.forEach((person) => {
       const personFig = document.createElement("figure");
       const personImg = document.createElement("img");
-      let charNum = getLastNumber(person.url);
-      personImg.src = `https://starwars-visualguide.com/assets/img/characters/.jpg`;
+      const charNum = extractCharNum(person.url);
+      personImg.src = `https://starwars-visualguide.com/assets/img/characters/${charNum}.jpg`;
       const personCap = document.createElement("figcaption");
       personCap.textContent = person.name;
-
       personFig.appendChild(personImg);
       personFig.appendChild(personCap);
       characterDiv.appendChild(personFig);
     });
+    displayText.textContent = "";
   }
+}
+
+function extractCharNum(url) {
+  const regex = /\/(\d+)\/$/;
+  return url.match(regex)[1];
 }
 
 populateDOM(people);
--- people/people.js
+++ people/people.js
@@ -1,4 +1,4 @@
-[
+export const people = [
     {
         "name": "Luke Skywalker",
         "height": "172",
